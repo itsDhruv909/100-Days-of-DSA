2dd6eb858253dd15c69bf28cf76be25cc70e58ae
Dhruv <itsdhruv.g99@gmail.com>
2026-02-19 23:43:05 +0530

    Add files via upload

---
+++ Day19.c
@@ -0,0 +1,41 @@
+/*
+Problem: Given an array of integers, find two elements whose sum is closest to zero.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>  // For abs()
+
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements:\n");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int min_sum = arr[0] + arr[1];
+    int num1 = arr[0];
+    int num2 = arr[1];
+
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = i + 1; j < n; j++) {
+
+            int sum = arr[i] + arr[j];
+
+            if(abs(sum) < abs(min_sum)) {
+                min_sum = sum;
+                num1 = arr[i];
+                num2 = arr[j];
+            }
+        }
+    }
+
+    printf("Two elements whose sum is closest to zero: %d and %d\n", num1, num2);
+
+    return 0;
+}
